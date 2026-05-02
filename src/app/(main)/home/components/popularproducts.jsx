32be45c591e6697d4feb2809509220165eb1f906
Asif Ahmed <asifahmedakash32@gmail.com>
2026-05-02 14:13:35 +0600

    all page organized

--- src/app/(main)/home/components/popularproducts.jsx
+++ src/app/(main)/home/components/popularproducts.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+// 🔥 Popular Products
+// In this section show any 3 products from the json data you have created in step 2.
+// Each card must show:
+// Image
+// Product Name
+// Rating
+// Price
+// View Details button
+
+
 const PopularProducts = () => {
     return (
         <div>
--- src/app/(main)/home/page.jsx
+++ src/app/(main)/home/page.jsx
@@ -1,10 +1,19 @@
 // import React from 'react';
-
+import HeroSection from './components/HeroSection';
+import PopularProducts from './components/PopularProducts';
+// import Extra from './components/extra';
+import ExtraSection from './components/ExtraSection';
 
 const HomePage = () => {
     return (
         <div>
             this is home page
+
+            <HeroSection />
+
+            <PopularProducts />
+
+            <ExtraSection />
 
         </div>
     );
