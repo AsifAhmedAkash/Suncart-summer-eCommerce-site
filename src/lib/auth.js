57a784e62ee83ac60046ee9626b16978afbf8393
Asif Ahmed <asifahmedakash32@gmail.com>
2026-05-04 04:30:47 +0600

    used next link env

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -13,4 +13,9 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
     },
+    trustedOrigins: [
+        "http://localhost:3000",
+        process.env.BETTER_AUTH_URL,
+        process.env.NEXT_PUBLIC_APP_URL,
+    ].filter(Boolean),
 });
